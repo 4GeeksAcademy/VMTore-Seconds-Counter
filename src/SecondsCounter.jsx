41b2bcdc81560baa7933a6c6610c91367ede4fef
Veronica <veronicamt85@gmail.com>
2024-05-20 22:27:21 +0200

    Subiendo repo a GitHub con plantilla Vite

---
+++ src/SecondsCounter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SecondsCounter = (props) => {
+    return (
+
+        <div>
+            <p>Prueba link</p>
+        </div>
+    )
+}
+
+
+<SecondsCounter seconds={3434} />
+
+export default SecondsCounter;
+
+
+
